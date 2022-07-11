07dc8e79d44ec4c7c86b6b72c7d630e51b857019
Pedro Marques <ppmarq1@gmail.com>
2022-07-11 15:18:52 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,1 @@
-/* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals ( id serial NOT NULL, name VARCHAR(100) NOT NULL, date_of_birth DATE NOT NULL, escape_attempts serial NOT NULL, neutered bool NOT NULL, weight_kg decimal NOT NULL);
