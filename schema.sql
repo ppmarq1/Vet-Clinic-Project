18536d2a20e531e0c2f70f4446f072e29a360fff
Pedro Marques <ppmarq1@gmail.com>
2022-07-12 13:24:25 +0100

    Add column species

--- schema.sql
+++ schema.sql
@@ -1,1 +1,3 @@
 CREATE TABLE animals ( id serial NOT NULL, name VARCHAR(100) NOT NULL, date_of_birth DATE NOT NULL, escape_attempts serial NOT NULL, neutered bool NOT NULL, weight_kg decimal NOT NULL);
+
+alter table animals add column species VARCHAR(100);
