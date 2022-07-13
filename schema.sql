7d719e4fa3bd5c290f767d1a397b42aa1d184502
Pedro Marques <ppmarq1@gmail.com>
2022-07-13 09:39:53 +0100

    Add new tables

--- schema.sql
+++ schema.sql
@@ -1,3 +1,8 @@
 CREATE TABLE animals ( id serial NOT NULL, name VARCHAR(100) NOT NULL, date_of_birth DATE NOT NULL, escape_attempts serial NOT NULL, neutered bool NOT NULL, weight_kg decimal NOT NULL, PRIMARY KEY (id));
 
 alter table animals add column species VARCHAR(100);
+
+
+CREATE TABLE owners ( id serial PRIMARY KEY, full_name VARCHAR(100) NOT NULL, age INT NOT NULL);
+CREATE TABLE species ( id serial PRIMARY KEY, name VARCHAR(100) NOT NULL);
+
